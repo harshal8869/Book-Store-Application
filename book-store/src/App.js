5e1fc8b4c6f838b61145efb6eacb9f06cbf7788c
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-07-24 00:44:50 +0530

    Update App.js

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import Books from "./components/Book/Books";
 import About from "./components/About";
+
 import BookDetail from "./components/Book/BookDetail";
 
 function App() {
